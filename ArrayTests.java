8d299bdf9a9c287e1ae9a856ce5cf7cf8e929039
Lele660 <60441401+Lele660@users.noreply.github.com>
2022-10-12 15:30:30 -0700

    changed array methods

--- ArrayTests.java
+++ ArrayTests.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.*;
+
 import org.junit.*;
 
 public class ArrayTests {
@@ -15,4 +16,18 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReversedInPlace2(){
+    int[] input1 = {1, 2, 3, 4};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{4, 3, 2, 1}, input1);
+  }
+
+  @Test
+  public void testReversed2(){
+    int[] input1 = {1, 2, 3, 4};
+    assertArrayEquals(new int[]{4, 3, 2, 1}, ArrayExamples.reversed(input1));
+  }
+    
 }
