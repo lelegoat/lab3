ed406361c13c033fe20f00cfccb7a3274e802b6f
Lele660 <60441401+Lele660@users.noreply.github.com>
2022-10-14 17:10:19 -0700

    report 3

--- LinkedListTests.java
+++ LinkedListTests.java
@@ -5,7 +5,15 @@ import org.junit.*;
 public class LinkedListTests {
     @Test
     public void testAppend(){
-        LinkedList linkedList = new LinkedList()
+        LinkedList linkedList = new LinkedList();
+        linkedList.append(1);
+        linkedList.append(0);
+        linkedList.append(2);
+
+        assertEquals(1, linkedList.root.value);
+        assertEquals(0, linkedList.root.next.value);
+        assertEquals(2, linkedList.root.next.next.value);
+        assertEquals(3,linkedList.length());
     }
     
 }
